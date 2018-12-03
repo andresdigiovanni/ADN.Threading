ad0dcf8a7227542302c55b6402b06a340ff3381b
Andrés <andresdigiovanni@gmail.com>
2018-12-03 19:19:21 +0100

    Added TaskTimeout

---
+++ src/ADN.Threading/Models/TaskTimeout.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADN.Threading
+{
+    /// <summary>
+    /// A static class to execute asynchronous operations.
+    /// </summary>
+    /// Waits for all of the provided cancellable System.Threading.Tasks.Task objects
+        //     to complete execution within a specified time interval.
+    public static class TaskTimeout
+    {
+        /// <summary>
+        /// Waits for the provided operation to complete execution within a specified time interval.
+        /// </summary>
+        /// <param name="timeout">The number of milliseconds to wait.</param>
+        /// <param name="action">Operation on which to wait.</param>
+        public static void CompletesIn(int timeout, Action action)
+        {
+            var task = Task.Run(action);
+            var completedInTime = Task.WaitAll(new[] { task }, TimeSpan.FromMilliseconds(timeout));
+
+            if (task.Exception != null)
+            {
+                if (task.Exception.InnerExceptions.Count == 1)
+                {
+                    throw task.Exception.InnerExceptions[0];
+                }
+
+                throw task.Exception;
+            }
+
+            if (!completedInTime)
+            {
+                throw new TimeoutException($"Task did not complete in {timeout} milliseconds.");
+            }
+        }
+    }
+}
---
+++ tests/ADN.Threading.Tests/Models/TaskTimeoutTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Xunit;
+
+namespace ADN.Threading.Tests
+{
+    public class TaskTimeoutTest
+    {
+        private const int DELAY_MILLISECONDS = 100;
+        private const int EXCESS_DELAY_MILLISECONDS = 400;
+        private const int TIMEOUT_MILLISECONDS = 200;
+
+        [Fact]
+        public void CompletesIn()
+        {
+            DelayFunction(TIMEOUT_MILLISECONDS, DELAY_MILLISECONDS);
+        }
+
+        [Fact]
+        public void CompletesIn_Expception_Timeout()
+        {
+
+            Assert.Throws<TimeoutException>(() =>
+                DelayFunction(TIMEOUT_MILLISECONDS, EXCESS_DELAY_MILLISECONDS)
+            );
+        }
+
+        private void DelayFunction(int timeout, int delay)
+        {
+            TaskTimeout.CompletesIn(timeout, () =>
+            {
+                Thread.Sleep(delay);
+            });
+        }
+    }
+}
